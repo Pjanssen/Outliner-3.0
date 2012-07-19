b20f38eefc30c7533d9c40bd6e5eb2d1b0b01983
Pier Janssen <pier@threesixty.nl>
2012-07-19 21:41:25 +0200

    Modified SelectCommand to include layer selection

--- Outliner_Plugin/Commands/SelectCommand.cs
+++ Outliner_Plugin/Commands/SelectCommand.cs
@@ -4,20 +4,21 @@ using System.Linq;
 using System.Text;
 using Outliner.Scene;
 using Autodesk.Max;
+using MaxUtils;
 
 namespace Outliner.Commands
 {
 public class SelectCommand : Command
 {
-   private IINodeTab newSelection;
-   private IINodeTab prevSelection;
+   private IEnumerable<IMaxNodeWrapper> newSelection;
+   private IEnumerable<IMaxNodeWrapper> oldSelection;
 
    public SelectCommand(IEnumerable<IMaxNodeWrapper> nodes)
    {
       if (nodes == null)
          throw new ArgumentNullException("nodes");
 
-      this.newSelection = HelperMethods.ToIINodeTab(nodes);
+      this.newSelection = nodes;
    }
 
    public override string Description
@@ -27,30 +28,45 @@ public class SelectCommand : Command
 
    public override void Do()
    {
-      IInterface ip = MaxInterfaces.Global.COREInterface;
+      IInterface ip = MaxInterfaces.COREInterface;
+
+      //Store old selection.
       Int32 selNodeCount = ip.SelNodeCount;
-      this.prevSelection = MaxInterfaces.Global.INodeTabNS.Create();
+      List<IMaxNodeWrapper> oldSel = new List<IMaxNodeWrapper>();
       for (int i = 0; i < selNodeCount; i++)
       {
-         this.prevSelection.AppendNode(ip.GetSelNode(i), true, 0);
+         oldSel.Add(IMaxNodeWrapper.Create(ip.GetSelNode(i)));
       }
+      this.oldSelection = oldSel;
 
-      SelectNodes(ip, this.newSelection);
+      //Select new selection.
+      SelectCommand.SelectNodes(ip, this.newSelection);
    }
 
    public override void Undo()
    {
-      SelectNodes(MaxInterfaces.Global.COREInterface, this.prevSelection);
+      SelectCommand.SelectNodes(MaxInterfaces.COREInterface, this.oldSelection);
    }
 
-   protected static void SelectNodes(IInterface ip, IINodeTab nodes)
+   protected static void SelectNodes(IInterface ip, IEnumerable<IMaxNodeWrapper> nodes)
    {
       if (ip == null || nodes == null)
          return;
 
+      //Clear previous selection.
       ip.ClearNodeSelection(false);
-      if (nodes.Count > 0)
-         ip.SelectNodeTab(nodes, true, false);
+
+      //Select INodes.
+      IEnumerable<IMaxNodeWrapper> inodes = nodes.Where(n => n is IINodeWrapper);
+      if (inodes.Count() > 0)
+         ip.SelectNodeTab(HelperMethods.ToIINodeTab(inodes), true, false);
+
+      //Select Layers.
+      IEnumerable<IILayerWrapper> layers = nodes.Where(n => n is IILayerWrapper).Cast<IILayerWrapper>();
+      foreach (IILayerWrapper layer in layers)
+      {
+         layer.IILayerProperties.Select(true);
+      }
    }
 }
 }
