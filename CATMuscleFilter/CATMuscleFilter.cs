23d8baab6279b98c7c0462a998592398ba4c16f4
Pier Janssen <pier@threesixty.nl>
2012-08-24 09:53:48 +0200

    CAT Muscles filter

---
+++ CATMuscleFilter/CATMuscleFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Outliner.Scene;
+using Outliner.Filters;
+using Outliner.Plugins;
+using Autodesk.Max;
+
+namespace CATMuscleFilter
+{
+   [OutlinerPlugin(OutlinerPluginType.Filter)]
+   [LocalizedDisplayName(typeof(Resources), "CATDisplayName")]
+   [FilterCategory(FilterCategories.Custom)]
+   public class CATMuscleFilter : Filter<IMaxNodeWrapper>
+   {
+      private const uint strandIDA = 0x7CF3882;
+      private const uint strandIDB = 0x47F01642;
+      private const uint handleIDA = 0x141E37A3;
+      private const uint handleIDB = 0x2B362406;
+
+      public override bool ShowNode(IMaxNodeWrapper data)
+      {
+         if (data == null)
+            return false;
+
+         return MaxUtils.ClassIDHelpers.IsClass(data.WrappedNode, strandIDA, strandIDB) ||
+                MaxUtils.ClassIDHelpers.IsClass(data.WrappedNode, handleIDA, handleIDB);
+         //IINodeWrapper iinodeWrapper = data as IINodeWrapper;
+         //if (iinodeWrapper == null)
+         //   return false;
+
+         //IObject objRef = iinodeWrapper.IINode.ObjectRef;
+         //if (objRef == null || objRef.SuperClassID != SClass_ID.Helper)
+         //   return false;
+
+         //return MaxUtils.ClassIDHelpers.Equals(objRef.ClassID, strandIDA, strandIDB) ||
+         //       MaxUtils.ClassIDHelpers.Equals(objRef.ClassID, handleIDA, handleIDB);
+      }
+   }
+}
