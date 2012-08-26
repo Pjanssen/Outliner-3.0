ddf8ea1945ecbfb24d26bc95dd14c14534f14a1a
Pier Janssen <pier@threesixty.nl>
2012-08-26 20:03:05 +0200

    Moved HierarchyMode to own plugin assembly

---
+++ HierarchyMode/TreeViewDragDropHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WinForms = System.Windows.Forms;
+using Outliner.Scene;
+using Outliner.Commands;
+using Outliner.Controls.Tree;
+
+namespace Outliner.Modes.Hierarchy
+{
+public class TreeViewDragDropHandler : DragDropHandler
+{
+   public TreeViewDragDropHandler() : base(null) { }
+
+   public override bool AllowDrag
+   {
+      get { return false; }
+   }
+
+   public override bool IsValidDropTarget(WinForms::IDataObject dragData)
+   {
+      IEnumerable<TreeNode> draggedNodes = DragDropHandler.GetNodesFromDataObject(dragData);
+      if (draggedNodes == null)
+         return false;
+
+      return HelperMethods.GetMaxNodes(draggedNodes).All(n => n is IINodeWrapper);
+   }
+
+   public override WinForms::DragDropEffects GetDragDropEffect(WinForms::IDataObject dragData)
+   {
+      if (this.IsValidDropTarget(dragData))
+         return WinForms::DragDropEffects.Move;
+      else
+         return TreeView.NoneDragDropEffects;
+   }
+
+   public override void HandleDrop(WinForms::IDataObject dragData)
+   {
+      if (!this.IsValidDropTarget(dragData))
+         return;
+
+      IEnumerable<TreeNode> draggedNodes = DragDropHandler.GetNodesFromDataObject(dragData);
+      if (draggedNodes == null)
+         return;
+
+      MoveMaxNodeCommand cmd = new MoveMaxNodeCommand(HelperMethods.GetMaxNodes(draggedNodes), null,
+         Resources.Command_Link, Resources.Command_Unlink);
+      cmd.Execute(true);
+   }
+}
+}
---
+++ OutlinerActions/OpenHierarchyMode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Outliner.Modes;
+using Outliner.Controls;
+using Outliner.Controls.Tree.Layout;
+using Outliner.Controls.Tree;
+using Outliner.Modes.Hierarchy;
+
+namespace Outliner.Actions
+{
+   public class OpenHierarchyModeAction : OpenOutlinerAction
+   {
+      public override string ActionText
+      {
+         get { return Resources.OpenHierarchy; }
+      }
+
+      protected override TreeMode CreateTreeController1(TestControl mainControl)
+      {
+         mainControl.treeView1.TreeNodeLayout = new TreeNodeLayout(OutlinerGUP.Instance.Layout);
+         mainControl.treeView1.Colors = TreeViewColorScheme.MayaColors; //OutlinerGUP.Instance.ColorScheme;
+
+         mainControl.treeView1.NodeSorter = new Outliner.NodeSorters.AlphabeticalSorter();
+         return new HierarchyMode(mainControl.treeView1);
+      }
+
+      protected override TreeMode CreateTreeController2(TestControl mainControl)
+      {
+         mainControl.treeView2.TreeNodeLayout = new TreeNodeLayout(OutlinerGUP.Instance.Layout);
+         mainControl.treeView2.Colors = TreeViewColorScheme.MayaColors;
+
+         mainControl.treeView2.NodeSorter = new Outliner.NodeSorters.AlphabeticalSorter();
+         return new HierarchyMode(mainControl.treeView2);
+      }
+   }
+}
