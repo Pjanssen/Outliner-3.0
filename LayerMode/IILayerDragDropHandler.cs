0d430afd3d59592f0ff508972d3e5ceafc69b262
Pier Janssen <pier@threesixty.nl>
2012-08-26 22:36:16 +0200

    Moved LayerMode to plugin assembly

---
+++ LayerMode/IILayerDragDropHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WinForms = System.Windows.Forms;
+using Outliner.Scene;
+using Outliner.Commands;
+using Outliner.Controls.Tree;
+
+namespace Outliner.Modes.Layer
+{
+public class IILayerDragDropHandler : DragDropHandler
+{
+   private IILayerWrapper layer;
+   public IILayerDragDropHandler(IILayerWrapper data)
+      : base(data)
+   {
+      this.layer = data;
+   }
+
+   public override bool AllowDrag
+   {
+      get { return !this.layer.IsDefault; }
+   }
+
+   public override bool IsValidDropTarget(WinForms::IDataObject dragData)
+   {
+      IEnumerable<TreeNode> draggedNodes = DragDropHandler.GetNodesFromDataObject(dragData);
+      if (draggedNodes == null)
+         return false;
+
+      return this.Data.CanAddChildNodes(HelperMethods.GetMaxNodes(draggedNodes));
+   }
+
+   public override WinForms::DragDropEffects GetDragDropEffect(WinForms::IDataObject dragData)
+   {
+      if (this.IsValidDropTarget(dragData))
+         return WinForms::DragDropEffects.Copy;
+      else
+         return TreeView.NoneDragDropEffects;
+   }
+
+   public override void HandleDrop(WinForms::IDataObject dragData)
+   {
+      if (!this.IsValidDropTarget(dragData))
+         return;
+
+      IEnumerable<TreeNode> draggedNodes = DragDropHandler.GetNodesFromDataObject(dragData);
+      if (draggedNodes == null)
+         return;
+
+      MoveMaxNodeCommand cmd = new MoveMaxNodeCommand(HelperMethods.GetMaxNodes(draggedNodes), this.Data,
+         Resources.Command_AddToLayer, Resources.Command_UnlinkLayer);
+      cmd.Execute(true);
+   }
+}
+}
