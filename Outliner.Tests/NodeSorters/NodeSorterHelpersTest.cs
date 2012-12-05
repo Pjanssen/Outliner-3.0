1290544ab34f933228d7f09823789bd0460d07a7
Pier Janssen <pier@threesixty.nl>
2012-12-05 07:26:25 +0100

    Added NodeSOrterHelpersTests

---
+++ Outliner.Tests/NodeSorters/NodeSorterHelpersTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Outliner.NodeSorters;
+using Outliner.Controls.Tree;
+using Outliner.MaxUtils;
+
+namespace Outliner.Tests.NodeSorters
+{
+[TestClass]
+public class NodeSorterHelpersTest
+{
+   private class MockSorter : NodeSorter
+   {
+      protected override int InternalCompare(TreeNode x, TreeNode y)
+      {
+         return 0;
+      }
+   }
+
+   private class MockPropertySorter : NodePropertySorter
+   {
+      public MockPropertySorter(NodeProperty prop) 
+         : base(prop) { }
+   }
+
+   [TestMethod]
+   public void RequiresSortForTypeTest()
+   {
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(null, typeof(MockSorter)), "sorter parameter = null");
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(new MockSorter(), null), "type parameter = null");
+
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(new MockSorter(), typeof(MockPropertySorter)), "single filter, type not the same");
+      Assert.IsTrue(NodeSorterHelpers.RequiresSort(new MockSorter(), typeof(MockSorter)), "single filter, same type");
+
+      MockSorter a = new MockSorter();
+      a.SecondarySorter = new MockSorter();
+      a.SecondarySorter.SecondarySorter = new MockPropertySorter(NodeProperty.None);
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(a, typeof(Object)));
+      Assert.IsTrue(NodeSorterHelpers.RequiresSort(a, typeof(MockSorter)));
+      Assert.IsTrue(NodeSorterHelpers.RequiresSort(a, typeof(MockPropertySorter)));
+   }
+
+   [TestMethod]
+   public void RequiresSortForPropertyTest()
+   {
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(null, NodeProperty.AllEdges), "sorter parameter = null");
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(new MockSorter(), NodeProperty.Name), "not a property sorter");
+
+      MockPropertySorter a = new MockPropertySorter(NodeProperty.Name);
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(a, NodeProperty.None), "none property");
+      Assert.IsFalse(NodeSorterHelpers.RequiresSort(a, NodeProperty.PrimaryVisibility), "non-matching property");
+      Assert.IsTrue(NodeSorterHelpers.RequiresSort(a, NodeProperty.Name), "exact matching property");
+      Assert.IsTrue(NodeSorterHelpers.RequiresSort(a, NodeProperty.Name | NodeProperty.IsHidden | NodeProperty.IsFrozen), "combination of properties");
+
+      MockSorter b = new MockSorter();
+      b.SecondarySorter = new MockPropertySorter(NodeProperty.IsHidden);
+      b.SecondarySorter.SecondarySorter = new MockPropertySorter(NodeProperty.Name);
+      Assert.IsTrue(NodeSorterHelpers.RequiresSort(b, NodeProperty.Name), "matching property in nested sorter");
+   }
+}
+}
