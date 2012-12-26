c54bcbadecd262e84712c328c0710e5e9d4047ce
Pier Janssen <pier@threesixty.nl>
2012-12-26 15:44:12 +0100

    Added version data to projects

--- TreeNodeButtons/Properties/AssemblyInfo.cs
+++ TreeNodeButtons/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TreeNodeButtons")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("TreeNodeButtons")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TreeNodeButtons Outliner Plugin")]
+[assembly: AssemblyCopyright("Copyright © Pier Janssen 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.2.12361")]
+[assembly: AssemblyFileVersion("3.0.2.12361")]
