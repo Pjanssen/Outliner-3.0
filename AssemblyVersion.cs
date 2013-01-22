3d2476fe866d2f304e64373bd6edb9f85b00b5f3
Pier Janssen <pier@threesixty.nl>
2013-01-22 22:55:48 +0100

    3.0.22

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.21.1320")]
-[assembly: AssemblyFileVersion("3.0.21.1320")]
+[assembly: AssemblyVersion("3.0.22.1322")]
+[assembly: AssemblyFileVersion("3.0.22.1322")]
 
 
