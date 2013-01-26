7d35c169ade1aac57851d77413016b37c8282720
Pier Janssen <pier@threesixty.nl>
2013-01-26 20:20:35 +0100

    Replaced MoveMaxNodeCommand

---
+++ Outliner.Core/Commands/UnlinkNodesCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Outliner.Scene;
+
+namespace Outliner.Commands
+{
+   public class UnlinkNodesCommand : Command
+   {
+      private IEnumerable<IMaxNode> nodes;
+      private String description;
+
+      public UnlinkNodesCommand(IEnumerable<IMaxNode> nodes, String description)
+      {
+         this.nodes = nodes;
+         this.description = description;
+      }
+
+      public override string Description
+      {
+         get { return this.description; }
+      }
+
+      public override void Do()
+      {
+         this.nodes.ForEach(RemoveFromParent);
+      }
+
+      private static void RemoveFromParent(IMaxNode node)
+      {
+         IMaxNode parent = node.Parent;
+         if (parent != null)
+            parent.RemoveChildNode(node);
+      }
+   }
+}
