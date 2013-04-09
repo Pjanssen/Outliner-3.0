67909fea40a1439cddbd85247c043a712f751880
Pier Janssen <pier@threesixty.nl>
2013-04-10 00:00:42 +0200

    Added INodeWrapperFactoryTests

---
+++ Outliner.Tests/Scene/INodeWrapperFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Autodesk.Max;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Outliner.Scene;
+
+namespace Outliner.Tests.Scene
+{
+   [TestClass]
+   public class INodeWrapperFactoryTests
+   {
+      [TestMethod]
+      [ExpectedException(typeof(ArgumentNullException))]
+      public void CreateMaxNode_Null_ThrowsException()
+      {
+         INodeWrapperFactory factory = new INodeWrapperFactory();
+         factory.CreateMaxNode(null);
+      }
+
+      [TestMethod]
+      public void CreateMaxNode_NonIINode_ReturnsNull()
+      {
+         INodeWrapperFactory factory = new INodeWrapperFactory();
+         IMaxNode result = factory.CreateMaxNode(42);
+         Assert.IsNull(result);
+      }
+
+      [TestMethod]
+      public void CreateMaxNode_IINode_ReturnsIMaxNode()
+      {
+         Mock<IINode> mockIINode = new Mock<IINode>();
+         INodeWrapperFactory factory = new INodeWrapperFactory();
+         IMaxNode result = factory.CreateMaxNode(mockIINode.Object);
+
+         Assert.IsNotNull(result);
+         Assert.AreEqual(mockIINode.Object, result.BaseObject);
+      }
+   }
+}
