d0bd770569d36018d5e40aa5357163168a88f8b4
Pier Janssen <pier@threesixty.nl>
2013-04-14 00:06:55 +0200

    Added tests for NodePropertyFilter

--- Outliner.Core/Filters/NodePropertyFilter.cs
+++ Outliner.Core/Filters/NodePropertyFilter.cs
@@ -10,18 +10,33 @@ using Outliner.MaxUtils;
 
 namespace Outliner.Filters
 {
+   /// <summary>
+   /// A Filter for filtering IMaxNodes by a node property value.
+   /// </summary>
    [OutlinerPlugin(OutlinerPluginType.Filter)]
    [LocalizedDisplayName(typeof(OutlinerResources), "Filter_NodeProperty")]
    public class NodePropertyFilter : Filter<IMaxNode>
    {
       private BooleanNodeProperty property;
 
+      /// <summary>
+      /// Initializes a new instance of the NodePropertyFilter class.
+      /// </summary>
       public NodePropertyFilter() : this(BooleanNodeProperty.None) { }
+
+      /// <summary>
+      /// Initializes a new instance of the NodePropertyFilter class using
+      /// the given BooleanNodeProperty.
+      /// </summary>
+      /// <param name="property">The node property to filter by.</param>
       public NodePropertyFilter(BooleanNodeProperty property)
       {
          this.property = property;
       }
 
+      /// <summary>
+      /// Gets or sets the node property to filter IMaxNodes by.
+      /// </summary>
       [XmlAttribute("property")]
       [DefaultValue(BooleanNodeProperty.None)]
       public BooleanNodeProperty Property 
---
+++ Outliner.Tests/Filters/NodePropertyFilterTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Outliner.Filters;
+using Outliner.MaxUtils;
+using Outliner.Scene;
+
+namespace Outliner.Tests.Filters
+{
+   [TestClass]
+   public class NodePropertyFilterTests
+   {
+      [TestMethod]
+      public void Property_SetNewValue_RaisesFilterChangedEvent()
+      {
+         Boolean filterChangedEventRaised = false;
+         NodePropertyFilter filter = new NodePropertyFilter();
+         filter.FilterChanged += (sender, args) => filterChangedEventRaised = true;
+
+         filter.Property = BooleanNodeProperty.BoxMode;
+
+         Assert.IsTrue(filterChangedEventRaised);
+      }
+
+      [TestMethod]
+      public void ShowNode_GetNodePropertyReturningTrue_ReturnsTrue()
+      {
+         NodePropertyFilter filter = new NodePropertyFilter(BooleanNodeProperty.BoxMode);
+         Mock<IMaxNode> node = new Mock<IMaxNode>();
+         node.Setup(n => n.GetNodeProperty(BooleanNodeProperty.BoxMode)).Returns(true);
+
+         Boolean result = filter.ShowNode(node.Object);
+
+         Assert.IsTrue(result);
+         node.Verify(n => n.GetNodeProperty(BooleanNodeProperty.BoxMode), Times.Once);
+      }
+
+      [TestMethod]
+      public void ShowNode_GetNodePropertyReturningFalse_ReturnsFalse()
+      {
+         NodePropertyFilter filter = new NodePropertyFilter(BooleanNodeProperty.BoxMode);
+         Mock<IMaxNode> node = new Mock<IMaxNode>();
+         node.Setup(n => n.GetNodeProperty(BooleanNodeProperty.BoxMode)).Returns(false);
+
+         Boolean result = filter.ShowNode(node.Object);
+
+         Assert.IsFalse(result);
+         node.Verify(n => n.GetNodeProperty(BooleanNodeProperty.BoxMode), Times.Once);
+      }
+   }
+}
