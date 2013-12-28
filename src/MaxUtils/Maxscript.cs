05f0993de01c1e11515f46bd141a345c20fbe787
Pier Janssen <pier@pjanssen.nl>
2013-12-28 11:37:34 +0100

    Removed redundant Maxscript.WriteToListener methods

--- src/MaxUtils/Maxscript.cs
+++ src/MaxUtils/Maxscript.cs
@@ -34,35 +34,5 @@ namespace Outliner.MaxUtils
             MaxInterfaces.Global.ExecuteMAXScriptScript(script, true, null);
          }
       }
-
-      /// <summary>
-      /// Writes a message to the maxscript listener.
-      /// </summary>
-      /// <param name="text">The text to write.</param>
-      public static void WriteToListener(String text)
-      {
-         MaxInterfaces.Global.TheListener.EditStream.Wputs(text + "\n");
-         MaxInterfaces.Global.TheListener.EditStream.Wflush();
-      }
-
-      /// <summary>
-      /// Writes the string representation of an object to the maxscript listener.
-      /// </summary>
-      /// <param name="obj">The object to write.</param>
-      public static void WriteToListener(Object obj)
-      {
-         Maxscript.WriteToListener(obj.ToString());
-      }
-
-      /// <summary>
-      /// Writes a message to the maxscript listener using String.Format.
-      /// </summary>
-      /// <param name="format">The format string.</param>
-      /// <param name="args">The objects to format.</param>
-      public static void WriteToListener(String format, params Object[] args)
-      {
-         String text = String.Format(CultureInfo.InvariantCulture, format, args);
-         Maxscript.WriteToListener(text);
-      }
    }
 }
