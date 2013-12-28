83500bbe096b240a8adf6bf7684d0ba026d6a4a7
Pier Janssen <pier@pjanssen.nl>
2013-12-28 10:01:58 +0100

    Added ListenerLogger

---
+++ src/MaxUtils/ListenerLogger.cs
@@ -0,0 +1,64 @@
+﻿using Autodesk.Max;
+using PJanssen.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Outliner.MaxUtils
+{
+   public class ListenerLogger : TextLogger
+   {
+      private const int InfoStyle = 0;
+      private const int DebugStyle = 1;
+      private const int ErrorStyle = 2;
+
+      private IListener listener;
+
+      public ListenerLogger(string toolName)
+         : this(toolName, LogTypes.All)
+      { }
+
+      public ListenerLogger(string toolName, LogTypes enabledTypes) 
+         : base(enabledTypes) 
+      {
+         this.listener = MaxInterfaces.Global.TheListener;
+
+         base.TimeFormat = "[" + toolName + "] ";
+      }
+
+      protected override void WriteToLog(LogTypes logType, string message)
+      {
+         SetListenerStyle(logType);
+
+         this.listener.EditStream.Wputs(message + Environment.NewLine);
+         this.listener.EditStream.Flush();
+
+         ResetListenerStyle();
+      }
+
+      private void SetListenerStyle(LogTypes logType)
+      {
+         switch (logType)
+         {
+            case LogTypes.Debug:
+               this.listener.SetStyle(DebugStyle);
+               break;
+            case LogTypes.Error:
+               this.listener.SetStyle(ErrorStyle);
+               break;
+            case LogTypes.Warning:
+               this.listener.SetStyle(ErrorStyle);
+               break;
+            default:
+               this.listener.SetStyle(InfoStyle);
+               break;
+         }
+      }
+
+      private void ResetListenerStyle()
+      {
+         this.listener.SetStyle(InfoStyle);
+      }
+   }
+}
